52376dea51f00180c7e6b4d75221d63d575bac96
Patrick Hanner <hannerpatrick@gmail.com>
2023-03-08 11:32:10 +0100

    added first example

---
+++ examples/communication/src/package.hpp
@@ -0,0 +1,12 @@
+#include "aglio/type_descriptor.hpp"
+#include <string>
+
+namespace package{
+    struct data{
+        int number;
+        float decimal;
+        std::string name;
+    }
+}
+
+#include "TypeDescriptor_package.hpp"
